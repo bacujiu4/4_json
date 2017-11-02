d0676edca4558b1a5f8269ef7b658a991852c9da
root <root@docker-srv.smc.loc>
2017-11-02 13:22:32 +0300

    fixed pep8 compliance errors

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,22 @@
 import json
 import argparse
 
+
 def load_data(file_path):
-    with open(file_path, 'r') as handler:
-        raw_json_data = handler.read()
+    with open(file_path, 'r') as opened_file:
+        raw_json_data = opened_file.read()
         parsed_json_data = json.loads(raw_json_data)
     return parsed_json_data
 
-def pretty_print_json(parsed_json_data):
-    pretty_json_data = json.dumps(parsed_json_data, indent=4, ensure_ascii = False)
+
+def prettify_json_data(parsed_json_data):
+    pretty_json_data = json.dumps(
+        parsed_json_data, indent=4, ensure_ascii=False)
     return pretty_json_data
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file_path')
     args = parser.parse_args()
-    print(pretty_print_json((load_data(args.file_path))))
+    print(prettfy_json_data(load_data(args.file_path)))
