2ec6a7a97b5be5d228b1659a249e80ab4534747b
root <root@docker-srv.smc.loc>
2017-11-02 11:29:46 +0300

    def modify

--- pprint_json.py
+++ pprint_json.py
@@ -4,15 +4,22 @@
 import json
 import argparse
 
-def load_data(filepath):
-     data = open(filepath, 'r').read()
-     print(pretty_print_json(data))
+def load_data(file_path):
+    with open(file_path, 'r') as handler:
+        raw_json_data = handler.read()
+        parsed_json_data = json.loads(raw_json_data)
+    return parsed_json_data
 
-def pretty_print_json(data):
-     return json.dumps(json.loads(data), indent=2, ensure_ascii = False)
+    #with open(file_path, 'r') as file_obj:
+    #    return json.loads(file_obj)
+
+def pretty_print_json(parsed_json_data):
+    pretty_json_data = json.dumps(parsed_json_data, indent=4, ensure_ascii = False)
+    return pretty_json_data
+    #return json.dumps(json.loads(), indent=2, ensure_ascii = False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('filepath')
+    parser.add_argument('file_path')
     args = parser.parse_args()
-    load_data(args.filepath)
+    print(pretty_print_json((load_data(args.file_path))))
